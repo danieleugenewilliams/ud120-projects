6d68fcd82a44b8006007446bf09a1e031be421da
danieleugenewilliams <danieleugenewilliams@gmail.com>
2017-02-07 00:52:46 -0800

    Add files via upload

---
+++ choose_your_own/your_algorithm.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+from prep_terrain_data import makeTerrainData
+from class_vis import prettyPicture
+from sklearn.metrics import accuracy_score
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+### the training data (features_train, labels_train) have both "fast" and "slow"
+### points mixed together--separate them so we can give them different colors
+### in the scatterplot and identify them visually
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+#### initial visualization
+plt.xlim(0.0, 1.0)
+plt.ylim(0.0, 1.0)
+plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.legend()
+plt.xlabel("bumpiness")
+plt.ylabel("grade")
+plt.show()
+################################################################################
+
+
+### your code here!  name your classifier object clf if you want the 
+### visualization code (prettyPicture) to show you the decision boundary
+
+# k nearest neighbor
+#from sklearn import neighbors
+#clf = neighbors.KNeighborsClassifier(weights='distance')
+# 0.932
+
+# random forest
+#from sklearn.ensemble import RandomForestClassifier
+#clf = RandomForestClassifier(n_estimators=10, min_samples_split=100)
+# 0.924
+
+# adaboost
+from sklearn.ensemble import AdaBoostClassifier
+#from sklearn.tree import DecisionTreeClassifier
+clf = AdaBoostClassifier()
+# 0.924
+
+# fit the data
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+print "accuracy:", accuracy_score(pred, labels_test)
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
